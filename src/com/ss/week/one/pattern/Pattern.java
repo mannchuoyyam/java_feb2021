222a4969fd9706b5cf12b4e1b3e6fcd773496495
mann <mann@mann-VirtualBox>
2021-02-23 10:58:25 -0800

    Using for loop instead of hard coded. Fixed pattern 1,2,and 3.

--- src/com/ss/week/one/pattern/Pattern.java
+++ src/com/ss/week/one/pattern/Pattern.java
@@ -10,33 +10,57 @@ package com.ss.week.one.pattern;
 
 public class Pattern {
 
+	public void printChars(char characterToBePrinted, int startAt, int numberOfCharacters) {
+		for(int i = 0; i < startAt; ++i) {
+			System.out.print(" ");
+		}
+		for(int i = 0; i < numberOfCharacters; ++i) {
+			System.out.print(characterToBePrinted);
+		}
+	}
 	public void printPatternOne() {
 		System.out.println("1)");
-		System.out.println("*");
-		System.out.println("**");
-		System.out.println("***");
-		System.out.println("****");
-		System.out.println("---------");
+		int numberOfLines = 5;
+		for(int line = 1; line <= numberOfLines; ++line) {
+			if(line != 5) {
+				printChars('*', 0, line);
+			}else {
+				printChars('-', 0, 9);
+				
+			}
+			System.out.println();
+		}
 		System.out.println();	
 	}
 	
 	public void printPatternTwo() {
 		System.out.println("2)");
-		System.out.println("----------");
-		System.out.println("****");
-		System.out.println("***");
-		System.out.println("**");
-		System.out.println("*");
+		int numberOfLines = 5;
+		for(int line = 1; line <= numberOfLines; ++line) {
+			if(line == 1) {
+				printChars('-', 0, 10);
+			}else {
+				printChars('*', 0, numberOfLines - line + 1);
+				
+			}
+			System.out.println();
+		}
 		System.out.println();
 	}
 	
 	public void printPatternThree() {
 		System.out.println("3)");
-		System.out.println("     *     ");
-		System.out.println("    ***    ");
-		System.out.println("   *****   ");
-		System.out.println("  *******  ");
-		System.out.println("-----------");
+		int numberOfLines = 5;
+		int startAt = 5;
+		for(int line = 1; line <= numberOfLines; ++line) {
+			if(line == numberOfLines) {
+				printChars('-', startAt--, 11);
+			}else {
+				printChars('*', startAt--, (2 * line) - 1);
+				
+			}
+			System.out.println();
+		}
 		System.out.println();
 	}
 	
